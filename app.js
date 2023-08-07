f493383060f5f1c370762f852f4a67e176c623e6
Summer <liq.qian.l@gmail.com>
2023-08-07 18:11:24 +0800

    update app.js to avoid key exposure

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const homeStartingContent = "Welcome to my journal webpage! You can compose and post your journal by clicking 'POST' button in the navigation bar. Anything you posted here can be retrieved anytime and shared with everyone. ";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const key = fs.readFileSync("mongoDBkey.txt", "utf-8");
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://liqqianl:Lq19950714@cluster0.kvvk5qk.mongodb.net/journalsDB");
+mongoose.connect("mongodb+srv://liqqianl:"+ key +"@cluster0.kvvk5qk.mongodb.net/journalsDB");
 
 const journalsSchema = mongoose.Schema({
   title: String,
